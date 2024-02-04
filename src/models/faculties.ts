829377b4aaa73150e7938629fff9359d937b838e
tushar-c23 <tusharc.cse@gmail.com>
2024-02-05 00:59:11 +0530

    MOD: Faculties model

---
+++ src/models/faculties.ts
@@ -0,0 +1,6 @@
+import {pgTable, serial, varchar, integer} from "drizzle-orm/pg-core";
+
+export const faculties = pgTable('faculties', {
+    id: serial('faculty_id').primaryKey(),
+    name: varchar('faculty_name')
+});
