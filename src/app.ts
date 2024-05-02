492ed12123083854439d072065d904d954c0f315
Priyanshu-agr <agarwal3014@gmail.com>
2024-05-03 01:56:52 +0530

    added event crud

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import authRoutes from "./modules/auth/auth.route";
+import eventRoutes from "./modules/events/event.route";
 import prisma from "./utils/prisma";
 import "dotenv/config"
 import { lookupStudentByEnrollmentNumber } from "./modules/auth/auth.service";
@@ -13,6 +14,8 @@ const port = process.env.PORT || 3000;
 app.get("/healthcheck", async (req, res) => {
     res.status(200).json({ status: "ok" });
 });
+
+app.use("/events", eventRoutes);
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
