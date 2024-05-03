73d324cad3ff0615840808099a73f6bc524624d6
Faizal Rahman <132841839+FaizalJnu@users.noreply.github.com>
2024-05-03 16:06:30 +0530

    club crud completed

--- src/modules/clubs/club.route.ts
+++ src/modules/clubs/club.route.ts
@@ -3,4 +3,10 @@ import * as clubController from "./club.controller";
 
 const router = express.Router();
 
+router.get("/", clubController.getAllClubs);
+router.get("/:clubId", clubController.getClubById);
+router.post("/", clubController.createClub);
+router.put("/:clubId", clubController.updateClub);
+router.delete("/:clubId", clubController.deleteClub);
+
 export default router;
